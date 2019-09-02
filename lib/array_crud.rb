9b0aa5061734f5b42a5a1814de3af941ca210d82
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-02 17:10:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,17 +22,19 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = []
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array =[]
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array[-1]
 end
