37a92d871caf39eae1925d0ca4971b4809d270a8
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-02 16:56:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,13 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["hermione", "harry", "ron", "dobby"]
-  array << "krookshanks"
+  array = []
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
- array = ["hermione", "harry", "ron", "dobby"]
- array.unshift "dumbledor"
+ array = []
+ array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
